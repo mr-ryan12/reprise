1952c0a0f57eaf349b6d57141be87712210c55fc
Ryan McBride <mr.ryan444@gmail.com>
2026-07-12 19:20:54 -0600

    Add exit processes to scripts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -95,7 +95,10 @@ async function main() {
 }
 
 main()
-  .then(() => prisma.$disconnect())
+  .then(async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  })
   .catch(async (e) => {
     console.error(e);
     await prisma.$disconnect();
--- prisma/update.ts
+++ prisma/update.ts
@@ -80,7 +80,10 @@ async function main() {
 }
 
 main()
-  .then(() => prisma.$disconnect())
+  .then(async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  })
   .catch(async (e) => {
     console.error(e);
     await prisma.$disconnect();
